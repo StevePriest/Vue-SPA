25c406de6157c0205a9519a065d17bc7dff29bda
Steve Priest <stevejpriest@yahoo.com>
2020-05-01 15:01:40 -0500

    Vue SPA

---
+++ src/App.vue
@@ -0,0 +1,110 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Vue.js SPA</h1>
+    </header>
+    <main>
+      <aside class="sidebar">
+      <div v-for="post in posts" :key="post.id">
+      {{ post.title }}
+  </div>
+</aside>
+<div class="content"></div>
+    </main>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      posts: null,
+      endpoint: 'https://jsonplaceholder.typicode.com/posts/',
+    }
+  },
+
+  created() {
+    this.getAllPosts();
+  },
+
+  methods: {
+    getAllPosts() {
+      axios.get(this.endpoint)
+        .then(response => {
+          this.posts = response.data;
+        })
+        .catch(error => {
+          console.log('-----error-------');
+          console.log(error);
+        })
+    }
+  }
+}
+</script>
+<script>
+  export default {
+    data () {
+      return {
+        posts: []
+      }
+    }
+  }
+</script>
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      posts: null,
+      endpoint: 'https://jsonplaceholder.typicode.com/posts/',
+    }
+  },
+
+  created() {
+    this.getAllPosts();
+  },
+
+  methods: {
+    getAllPosts() {
+      axios.get(this.endpoint)
+        .then(response => {
+          this.posts = response.data;
+        })
+        .catch(error => {
+          console.log('-----error-------');
+          console.log(error);
+        })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
